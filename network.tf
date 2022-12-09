a8c157c88d7ee7be7dad762dbbf5a3aa9ec7f364
rao-tenali <rao.tech28@gmail.com>
2022-12-09 14:02:59 -0800

    removed

--- network.tf
+++ network.tf
@@ -1,28 +1,28 @@
-resource "aws_internet_gateway" "igw"{
-    vpc_id  =   "${aws_vpc.vpc.id}"
+# resource "aws_internet_gateway" "igw"{
+#     vpc_id  =   "${aws_vpc.vpc.id}"
 
-    tags    =   {
-        Name    =   "igw"
-    }
-}
+#     tags    =   {
+#         Name    =   "igw"
+#     }
+# }
 
-resource "aws_route_table" "prod-public-crt" {
-    vpc_id = "${aws_vpc.vpc.id}"
+# resource "aws_route_table" "prod-public-crt" {
+#     vpc_id = "${aws_vpc.vpc.id}"
     
-    route {
-        //associated subnet can reach everywhere
-        cidr_block = "0.0.0.0/0" 
-        //CRT uses this IGW to reach internet
-        gateway_id = "${aws_internet_gateway.igw.id}" 
-    }
+#     route {
+#         //associated subnet can reach everywhere
+#         cidr_block = "0.0.0.0/0" 
+#         //CRT uses this IGW to reach internet
+#         gateway_id = "${aws_internet_gateway.igw.id}" 
+#     }
     
-    tags =  {
-        Name = "prod-public-crt"
-    }
-}
+#     tags =  {
+#         Name = "prod-public-crt"
+#     }
+# }
 
-resource "aws_route_table_association" "prod-crta-public-subnet-1"{
-    subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
-    route_table_id = "${aws_route_table.prod-public-crt.id}"
-}
+# resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+#     subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
+#     route_table_id = "${aws_route_table.prod-public-crt.id}"
+# }
 
--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,22 @@
-resource "aws_vpc" "vpc" {
-  cidr_block       = "${var.AWS_CIDRBLOCK}"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink = "false"
-  instance_tenancy = "default"
+# resource "aws_vpc" "vpc" {
+#   cidr_block       = "${var.AWS_CIDRBLOCK}"
+#   enable_dns_support = "true"
+#   enable_dns_hostnames = "true"
+#   enable_classiclink = "false"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "vpc"
-  }
-}
+#   tags = {
+#     Name = "vpc"
+#   }
+# }
 
-resource "aws_subnet" "prod-subnet-public-1" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "us-east-1a"
+# resource "aws_subnet" "prod-subnet-public-1" {
+#     vpc_id = "${aws_vpc.vpc.id}"
+#     cidr_block = "10.0.1.0/24"
+#     map_public_ip_on_launch = "true"
+#     availability_zone = "us-east-1a"
 
-    tags =  {
-        Name = "prod-subnet-public-1"
-    }
-}
+#     tags =  {
+#         Name = "prod-subnet-public-1"
+#     }
+# }
