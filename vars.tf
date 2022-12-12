ad8a925655f906249e20955f0d10ee9d9a2534ff
rao-tenali <rao.tech28@gmail.com>
2022-12-12 11:10:25 -0800

    loop changes

--- vars.tf
+++ vars.tf
@@ -6,3 +6,10 @@ variable "AWS_CIDRBLOCK"{
     default= "10.0.0.0/16"
 }
 
+variable "subnet_cidr"{
+    default= ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+
+variable "subnet_azs"{
+    default=["us-east-1a","us-east-1b","us-east-1c"]
+}
--- vpc.tf
+++ vpc.tf
@@ -1,22 +1,23 @@
-# resource "aws_vpc" "vpc" {
-#   cidr_block       = "${var.AWS_CIDRBLOCK}"
-#   enable_dns_support = "true"
-#   enable_dns_hostnames = "true"
-#   enable_classiclink = "false"
-#   instance_tenancy = "default"
+resource "aws_vpc" "vpc" {
+  cidr_block       = "${var.AWS_CIDRBLOCK}"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink = "false"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = "vpc"
-#   }
-# }
+  tags = {
+    Name = "vpc"
+  }
+}
 
-# resource "aws_subnet" "prod-subnet-public-1" {
-#     vpc_id = "${aws_vpc.vpc.id}"
-#     cidr_block = "10.0.1.0/24"
-#     map_public_ip_on_launch = "true"
-#     availability_zone = "us-east-1a"
 
-#     tags =  {
-#         Name = "prod-subnet-public-1"
-#     }
-# }
+resource "aws_subnet" "prod-subnet-public-1" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    cidr_block = "${element(var.subnet_cidr)}"
+    map_public_ip_on_launch = "true"
+    availability_zone = "${element(var.subnet_azs)}"
+
+    tags =  {
+        Name = "prod-subnet-public-1"
+    }
+}
