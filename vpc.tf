0255b6e5e6ac6420b588d20b1eded6f3b42bb0eb
rao-tenali <rao.tech28@gmail.com>
2022-12-12 11:16:36 -0800

    update

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc" {
   cidr_block       = "${var.AWS_CIDRBLOCK}"
   enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink = "false"
+  enable_classiclink = "true"
   instance_tenancy = "default"
 
   tags = {
