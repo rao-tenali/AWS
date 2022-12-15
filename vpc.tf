278e141f324f3023e6ecfaf68badde1dd24a84cd
rao-tenali <rao.tech28@gmail.com>
2022-12-15 13:41:34 -0800

    updated code

--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,23 @@
-resource "aws_vpc" "vpc" {
-  cidr_block       = "${var.AWS_CIDRBLOCK}"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
-  instance_tenancy = "default"
+# resource "aws_vpc" "vpc" {
+#   cidr_block       = "${var.AWS_CIDRBLOCK}"
+#   enable_dns_support = "true"
+#   enable_dns_hostnames = "true"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "vpc"
-  }
-}
+#   tags = {
+#     Name = "vpc"
+#   }
+# }
 
 
-resource "aws_subnet" "prod-subnet-public-1" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    count = length(var.subnet_azs)
-    cidr_block = "${element(var.subnet_cidr,count.index)}"
-    map_public_ip_on_launch = "true"
-    availability_zone = "${element(var.subnet_azs,count.index)}"
+# resource "aws_subnet" "prod-subnet-public-1" {
+#     vpc_id = "${aws_vpc.vpc.id}"
+#     count = length(var.subnet_azs)
+#     cidr_block = "${element(var.subnet_cidr,count.index)}"
+#     map_public_ip_on_launch = "true"
+#     availability_zone = "${element(var.subnet_azs,count.index)}"
 
-    tags =  {
-        Name = "${element(var.crt_names,count.index)}"
-    }
-}
+#     tags =  {
+#         Name = "${element(var.crt_names,count.index)}"
+#     }
+# }
