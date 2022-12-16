bffe5a7031d48cd3bc2700e39968c7fa4da18586
rao-tenali <rao.tech28@gmail.com>
2022-12-16 12:12:22 -0800

    update

--- sg.tf
+++ sg.tf
@@ -16,43 +16,44 @@
 #   security_group_id = aws_security_group.sg.id
 # }
 
-locals {
-  ports_in = [
-    80,
-    443,
-    22
-  ]
-  ports_out = [
-    0
-  ]
-}
+# 2nd option
+# locals {
+#   ports_in = [
+#     80,
+#     443,
+#     22
+#   ]
+#   ports_out = [
+#     0
+#   ]
+# }
 
-resource "aws_security_group" "internet_facing_alb" {
-  name        = "internetfacing-loadbalancer-sg"
-  description = "Security group attached to internet facing loadbalancer"
-  vpc_id = "${aws_vpc.vpc.id}"
+# resource "aws_security_group" "internet_facing_alb" {
+#   name        = "internetfacing-loadbalancer-sg"
+#   description = "Security group attached to internet facing loadbalancer"
+#   vpc_id = "${aws_vpc.vpc.id}"
 
-  dynamic "ingress" {
-    for_each = toset(local.ports_in)
-    content {
-      description = "Web Traffic from internet"
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  dynamic "egress" {
-    for_each = toset(local.ports_out)
-    content {
-      description = "Web Traffic to internet"
-      from_port   = egress.value
-      to_port     = egress.value
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  tags = {
-    Name = "internetfacing-loadbalancer-sg"
-  }
-}
+#   dynamic "ingress" {
+#     for_each = toset(local.ports_in)
+#     content {
+#       description = "Web Traffic from internet"
+#       from_port   = ingress.value
+#       to_port     = ingress.value
+#       protocol    = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+#   dynamic "egress" {
+#     for_each = toset(local.ports_out)
+#     content {
+#       description = "Web Traffic to internet"
+#       from_port   = egress.value
+#       to_port     = egress.value
+#       protocol    = "-1"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+#   tags = {
+#     Name = "internetfacing-loadbalancer-sg"
+#   }
+# }
