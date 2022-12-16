140eef104304ee3c7e7e977f28b20bb2f16c0c2c
rao-tenali <rao.tech28@gmail.com>
2022-12-16 11:56:53 -0800

    update

--- sg.tf
+++ sg.tf
@@ -15,3 +15,44 @@
 #   description       = var.sg_ingress_rules[count.index].description
 #   security_group_id = aws_security_group.sg.id
 # }
+
+locals {
+  ports_in = [
+    80,
+    443,
+    22
+  ]
+  ports_out = [
+    0
+  ]
+}
+
+resource "aws_security_group" "internet_facing_alb" {
+  name        = "internetfacing-loadbalancer-sg"
+  description = "Security group attached to internet facing loadbalancer"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  dynamic "ingress" {
+    for_each = toset(locals.ports_in)
+    content {
+      description = "Web Traffic from internet"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = toset(locals.ports_out)
+    content {
+      description = "Web Traffic to internet"
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  tags = {
+    Name = "internetfacing-loadbalancer-sg"
+  }
+}
